485b91acde05f90464d42bdb5c7dbb0f8e134629
Jeshwar0910 <jeshwarjes@gmail.com>
2024-02-15 19:43:50 +0530

    Modified StyleSheet

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-let links=document.getElementsByClassName("links");
-let contents=document.getElementsByClassName("contents");
+let links = document.getElementsByClassName("links");
+let contents = document.getElementsByClassName("contents");
 
-function display(tablink){
-    for(link of links ){
-        link.classList.remove("active-links");
-    }
-    for(content of contents ){
-        content.classList.remove("active-content");
-        event.currentTarget.classList.add("active-links");
-        document.getElementById(tablink).classList.add("active-content");
-    }}
+function display(tablink) {
+  for (link of links) {
+    link.classList.remove("active-links");
+  }
+  for (content of contents) {
+    content.classList.remove("active-content");
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tablink).classList.add("active-content");
+  }
+}
 
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzN-xUz0hLsNwjSKy2K4xfiZFz4JjTwWzvvWYMUI8he_nNXA7TBWvLE2mCuT1gajoE4kA/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-
-
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbzN-xUz0hLsNwjSKy2K4xfiZFz4JjTwWzvvWYMUI8he_nNXA7TBWvLE2mCuT1gajoE4kA/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg=document.getElementById("msg");
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML="Message Sent Successfully"
-        setTimeout(function(){
-            msg.innerHTML=""
-        },5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message Sent Successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
